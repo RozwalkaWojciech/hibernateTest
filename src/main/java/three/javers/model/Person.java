3bcbf4dd44e67232d70db318885b0b8568404772
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-08 22:09:27 +0100

    add lombok project

--- src/main/java/three/javers/model/Person.java
+++ src/main/java/three/javers/model/Person.java
@@ -1,11 +1,18 @@
 package three.javers.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.util.UUID;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Person {
 
