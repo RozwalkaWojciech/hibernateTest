34087b4484d089b67c5ab7ebd73b2c385f665c3d
Wojciech Rozwałka <wojciechrozwalka@gmail.com>
2020-12-08 22:27:39 +0100

    add full CRUD in personDao

--- src/main/java/three/javers/dao/PersonDao.java
+++ src/main/java/three/javers/dao/PersonDao.java
@@ -5,6 +5,7 @@ import three.javers.model.Person;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.UUID;
 
 @Stateless
 public class PersonDao {
@@ -15,5 +16,29 @@ public class PersonDao {
     public Person savePerson(Person person) {
         entityManager.persist(person);
         return person;
+    }
+
+    public Person findById(UUID id) {
+        return entityManager.find(Person.class, id);
+    }
+
+    public Person edit(UUID id, Person person) {
+        Person personToEdit = findById(id);
+        if (personToEdit != null) {
+            personToEdit.setName(person.getName());
+            personToEdit.setLastName(person.getLastName());
+            entityManager.merge(personToEdit);
+            return personToEdit;
+        }
+        return null;
+    }
+
+    public boolean remove(UUID id) {
+        Person personToRemove = findById(id);
+        if (personToRemove != null) {
+            entityManager.remove(personToRemove);
+            return true;
+        }
+        return false;
     }
 }
